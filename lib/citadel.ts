795e9f03aefedaa538a6d886beae670ec6d21aa3
Aaron Dewes <aaron.dewes@protonmail.com>
2021-11-17 17:24:58 +0100

    Support window.location.origin if BASE_URL is not set (#6)
    When this is launched, the base URL may be different depending on if the user accesses over Tor, citadel.local, or locally using the IP.
    By not depending on the base URL from process.env, this is fixed.

--- lib/citadel.ts
+++ lib/citadel.ts
@@ -1,5 +1,5 @@
 import { Citadel } from "@runcitadel/sdk/browser/index.browser.js";
 
-const citadel = new Citadel(process.env.BASE_URL);
+const citadel = new Citadel(process.env.BASE_URL || window.location.origin);
 
 export default citadel;
