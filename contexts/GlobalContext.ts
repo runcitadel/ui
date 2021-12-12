44ee04a115dafbdf967ddd6d8dfd84711600b12e
Aaron Dewes <aaron.dewes@protonmail.com>
2021-12-12 21:29:54 +0100

    Update dependencies (#12)
    * WIP: Update dependencies
    
    * Finalize migration by upgrading to Beta 14
    
    * Type workaround
    
    The import path will be fixed in the SDK soon
    
    * Update SDK to Beta 15 with bug fixes

--- contexts/GlobalContext.ts
+++ contexts/GlobalContext.ts
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-import { Citadel } from "@runcitadel/sdk/browser/index.browser";
+import Citadel from "@runcitadel/sdk/browser/index.js";
 
 export const GlobalContext = createContext<Citadel | null>(null);
